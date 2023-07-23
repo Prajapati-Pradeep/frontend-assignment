178c895aca1546dc43a350bd2e0d6cb5a95b825c
Pradeep Prajapati <prajapatipradeep56@gmail.com>
2023-07-23 10:38:47 +0545

    serch page navigation added in header

--- online-store/src/components/Header/index.jsx
+++ online-store/src/components/Header/index.jsx
@@ -1,9 +1,13 @@
 import { Link } from "react-router-dom"
+import styles from './styles.module.css'
 const Header = () => {
     return (
-        <Link to='/' style={{color:'inherit'}}>
-            <h2 className="m-0 pt-4 pb-4 text-center" style={{backgroundColor:'#EF694C'}}>Online Sore</h2>
-        </Link>
+        <div className={styles.header}>
+            <Link to={'/'} >
+                <h2 className="m-0 pt-4 pb-4 text-center" >Online Sore</h2>
+            </Link>
+            <Link to={'/search'} className={styles.search}>  Search</Link>
+        </div>
     )
 }
 
